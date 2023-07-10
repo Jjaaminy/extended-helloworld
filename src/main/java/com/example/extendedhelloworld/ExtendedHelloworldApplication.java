203bec8b47cf1dc3a3601d2964b329d0204ebfe3
Jjaaminy <jaaminy18@gmail.com>
2023-07-10 10:49:07 +0200

    Initial commit

---
+++ src/main/java/com/example/extendedhelloworld/ExtendedHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.extendedhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtendedHelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/extendedhelloworld/ExtendedHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extendedhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendedHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
